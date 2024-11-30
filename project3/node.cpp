7842c446e72403639e3f0960a40503d612946feb
alisianguyen <139920651+alisianguyen@users.noreply.github.com>
2024-11-30 17:43:37 -0500

    updating node.cpp with shell sort

--- project3/node.cpp
+++ project3/node.cpp
@@ -37,3 +37,19 @@ void quickSort(vector<node>& array, int low, int high){
     }
 }
 
+// Shell sort from Module 8 Discussion Slides
+void shellSort(vector<node>& array){
+    int n = array.size();
+    for (int gap = n/2; gap > 0; gap /= 2){
+        for (int i = gap; i < ; i++){
+            node temp = array[i];
+            int j;
+            for (j = i; j >= gap && array[j - gap] > temp; j -= gap){
+                array[j] = array[j - gap];
+            }
+        }
+        array[j] = temp;
+    }
+    return 0;
+}
+
